071ad06a258aa96c5d1fc426ab6d4d73192f24f6
Mosleymos - MKE <keitamori@gmail.com>
2018-07-28 20:33:03 +0200

    Elements pour creer styles - essais en cours

---
+++ src/button.js
@@ -0,0 +1,3 @@
+const makeButton = buttonName => `Button : ${buttonName}`
+
+export { makeButton }
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import nav from './nav';
+import { top, bottom } from './footer';
+
+import { makeButton } from './button';
+
+import { red, blue, makeColorStyle } from './button-styles';
+
+console.log(nav(), top, bottom, makeButton('my first button'), makeColorStyle("mustard"))
