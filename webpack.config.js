b3c2c8f3906e5af01f5fd5df37684399af868b6f
Mosleymos - MKE <keitamori@gmail.com>
2018-07-29 12:12:48 +0200

    Configuration de base

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-module.exports = (env) => {
 
-  console.log(env)
-    return {
-      //mode: env.mode,
+const webpackMerge = require("webpack-merge");
+
+module.exports = ({mode, presets}= {mode: "production", presets: []}) => {
+
+  console.log(mode)
+    return webpackMerge({
+      mode,
       output:{
         filename: "bundle.js"
       },
       plugins:[new HtmlWebPackPlugin(), new webpack.ProgressPlugin()]
-
-    };
+    },
+    //modeConfig(mode),
+    //loadPresets({mode, presets})
+    )
+    // return {
+    //   //mode: env.mode,
+    //   mode,
+    //   output:{
+    //     filename: "bundle.js"
+    //   },
+    //   plugins:[new HtmlWebPackPlugin(), new webpack.ProgressPlugin()]
+    //
+    // };
 };
