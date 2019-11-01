40c57f8785f8af7e96ed5ae7ef547b846c8d6eea
mori keita <keitamori@gmail.com>
2019-11-01 21:08:25 +0100

    Servicee worker ? Test

---
+++ csr4data/public/service-worker.js
@@ -0,0 +1,40 @@
+// https://www.hacklabo.com/your-first-service-worker/
+var cacheName = 'your-first-service-worker';
+
+// var urlsToCache = [
+//     '/',
+//     'css/tachyons.min.css',
+//     'img/andre-benz-248755.jpg',
+//     'img/andre-benz-250740.jpg',
+//     'img/andre-benz-256762.jpg',
+//     'img/redd-angelo-230297.jpg'
+// ];
+
+var urlsToCache = [
+  "/articles.json"
+]
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch the contents and reply with cache
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            debugger
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
